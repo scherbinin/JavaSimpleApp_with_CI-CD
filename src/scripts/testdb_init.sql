f0e8afbbfa0a6be26f34d452852890ca12303d99
ilsc0215 <scherbininiliy@gmail.com>
2018-05-16 17:39:30 +0300

    Debug sql

--- src/scripts/testdb_init.sql
+++ src/scripts/testdb_init.sql
@@ -2,9 +2,3 @@ CREATE DATABASE testing_db;
 
 CREATE USER test WITH PASSWORD 'test';
 
-CREATE TABLE public.test_table
-(
-  id SERIAL PRIMARY KEY,
-  comment VARCHAR
-);
-CREATE UNIQUE INDEX test_table_id_uindex ON public.test_table (id);
