486dbc3e1f8b8320494288199189192a43c29649
ilsc0215 <scherbininiliy@gmail.com>
2018-05-16 17:50:28 +0300

    Debug sql

--- src/scripts/testdb_init.sql
+++ src/scripts/testdb_init.sql
@@ -1,4 +1,7 @@
-CREATE DATABASE testing_db;
+CREATE TABLE testing_db.public.test_table
+(
+  id SERIAL PRIMARY KEY,
+  comment VARCHAR
+);
 
-CREATE USER test WITH PASSWORD 'test';
-
+CREATE UNIQUE INDEX test_table_id_uindex ON public.test_table (id);
