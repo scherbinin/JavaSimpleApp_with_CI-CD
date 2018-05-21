df0323d45faa28f40ce264c3b1bed08db3c4817a
ilsc0215 <scherbininiliy@gmail.com>
2018-05-21 13:38:27 +0300

    SQL TEST DB init script added

---
+++ src/main/resources/scripts/init_test_env.sql
@@ -0,0 +1,5 @@
+-- The root user will be used: login: postgres, pswrd: ""
+
+CREATE DATABASE testing_db;
+CREATE TABLE testing_db.public.test_table (id SERIAL PRIMARY KEY, comment VARCHAR);
+CREATE UNIQUE INDEX test_table_id_uindex ON testing_db.public.test_table (id);
